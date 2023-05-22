366d2404f27e24762ca82eb66c3df63c052c6893
sabo550 <saborahmatu866@gmail.com>
2023-05-22 01:24:49 -0700

    get_arg.c

--- get_arg.c
+++ get_arg.c
@@ -4,6 +4,20 @@
  * main - prints all arguments it receives
  * @ac: number of arguments
  * @av: array of arguments
- * 
+ *
  * Return: Always 0(success)
  */
+
+int main(int ac, char **av)
+{
+	(void)ac;
+	int i = 0;
+
+	while (av[i] != NULL)
+	{
+		printf("%s\n", av[i]);
+		i++;
+	}
+
+	return (0);
+}
