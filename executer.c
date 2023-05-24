14a137c350e146250e4c58f433ac5aedbbf130d5
BABABUSOLA <queenbusola1@gmail.com>
2023-05-24 14:11:49 -0700

    update simple shell

---
+++ executer.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void executer(char **argv){
+	char *cmd = NULL, *cmd_main = NULL;
+	
+	if (argv)
+	{
+		cmd = argv[0];/* get the command */
+
+        /* generate the path to this command before passing it to execve */
+		cmd_main = getPath(cmd);
+
+        /* execute the actual command with execve */
+		if (execve(cmd_main, argv, NULL) == -1){
+			perror("Error:");
+		}
+	}
+}
