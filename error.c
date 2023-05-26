d2ca7334b53f8e799d7254dd5e995dca76108465
BABABUSOLA <queenbusola1@gmail.com>
2023-05-26 14:51:45 -0700

    push the changes

---
+++ error.c
@@ -0,0 +1,58 @@
+#include  "main_shell.h"
+
+/**
+ * print_d - function prints a decimal (integer) number (base 10)
+ * @input: input
+ * @fd: filedescriptor to write to
+ *
+ * Return: number of characters printed
+ */
+int print_d(int input, int fd)
+{
+	int (*__putchar)(char) = _putchar;
+	int i, count = 0;
+	unsigned int _abs_, current;
+
+	if (fd == STDERR_FILENO)
+		__putchar = _putchar;
+	if (input < 0)
+	{
+		_abs_ = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (i = 1000000000; i > 1; i /= 10)
+	{
+		if (_abs_ / i)
+		{
+			__putchar('0' + current / i);
+			count++;
+		}
+		current %= i;
+	}
+	__putchar('0' + current);
+	count++;
+
+	return (count);
+}
+
+/**
+ * print_error - prints an error message
+ * @info: the parameter & return info struct
+ * @estr: string containing specified error type
+ * Return: 0 if no numbers in string, converted number otherwise
+ *        -1 on error
+ */
+void print_error(char *fname, int line_count, char **argv, char *estr)
+{
+	_puts(fname);
+	_puts(": ");
+	print_d(line_count, STDERR_FILENO);
+	_puts(": ");
+	_puts(argv[0]);
+	_puts(": ");
+	_puts(estr);
+}
--- main_shell.h
+++ main_shell.h
@@ -1,6 +1,7 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,15 +11,47 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+/* for read/write buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+/* for convert_number() */
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+/* 1 if using system getline() */
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
+
+extern char **environ;
+
 int _strlen(const char *s);
 char *_strcpy(char *dest, const char *src);
 char *_strdup(const char *string);
 char **splitString(const char *string, int *wordCount);
 char **strtow(char *str);
+int _strcmp(char *s1, char *s2);
+int _atoi(char *s);
+int print_d(int input, int fd);
+void _puts(char *str);
+int _putchar(char c);
+int _putfd(char c, int fd);
+int _putsfd(char *str, int fd);
+
+/*converter.c */
+char *convert_number(long int num, int base, int flags);
+void remove_comments(char *buf);
 
 ssize_t my_getline(char **buff, size_t *length);
 
 int executer(char **argv);
 char *getPath(char *command);
+
+/*error.c*/
+void print_error(char *fname, int line_count, char **argv, char *estr);
 
 #endif
